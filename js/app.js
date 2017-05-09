a49ade01207c60651788aed4ee8c26f4cd8087e5
David Thorpe <davidthorpe878@gmail.com>
2017-05-09 19:08:59 +0100

    Finished updated catclicker
    Using KnockoutJS

--- js/app.js
+++ js/app.js
@@ -1,26 +1,89 @@
-var ViewModel = function() {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Cool D');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.imgAttribution = ko.observable('https://www.flicker.com/photos/big');
+var initialCats = [
+        {
+            clickCount : 0,
+            name : 'Tabby',
+            imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+            nicknames: ['thekat', 'dumbo']
+        },
+        {
+            clickCount : 0,
+            name : 'Tiger',
+            imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+            nicknames: ['sleepy']
+        },
+        {
+            clickCount : 0,
+            name : 'Scaredy',
+            imgSrc : 'img/22252709_010df3379e_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
+            nicknames: ['coldy']
+        },
+        {
+            clickCount : 0,
+            name : 'Shadow',
+            imgSrc : 'img/1413379559_412a540d29_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+            nicknames: ['katty']
+        },
+        {
+            clickCount : 0,
+            name : 'Sleepy',
+            imgSrc : 'img/9648464288_2516b35537_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
+            nicknames: ['hello']
+        }
+    ];
 
-	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
-	};
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgSrc);
+	this.nicknames = ko.observableArray(data.nicknames);
 
-	this.level = ko.computed(function () {
+	this.title = ko.computed(function () {
+		var title;
 		var clicks = this.clickCount();
 
 		if (clicks < 5) {
-			return "infant"
+			title = 'infant';
 		}
-		else if (clicks > 4 && clicks < 10) {
-			return "child"
+		else if (clicks < 10) {
+			title = 'child';
+		}
+		else if (clicks < 15) {
+			title = 'teen';
+		}
+		else if (clicks < 20) {
+			title = 'adult';
 		}
 		else {
-			return "adult"
-		};
+			title = 'OAP';
+		}
+		return title;
 	}, this);
 }
+
+var ViewModel = function() {
+	var self = this;
+
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catItem) {
+		self.catList.push( new Cat(catItem) );
+	});
+
+	this.currentCat = ko.observable( this.catList()[0] );
+	
+	this.incrementCounter = function() {
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+	};
+
+	this.setCat = function(clickedCat) {
+		self.currentCat(clickedCat);
+	};
+};
 
 ko.applyBindings(new ViewModel());
